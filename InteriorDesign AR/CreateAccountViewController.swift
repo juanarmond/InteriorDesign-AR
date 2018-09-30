ebde1648454da1543af5b15773cda3121634b0c7
Juan Armond <juan_armond@hotmail.com>
2018-09-30 09:29:21 +0100

    Welcome screen +  icons

---
+++ InteriorDesign AR/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  InteriorDesign AR
+//
+//  Created by Juan Armond on 30/09/2018.
+//  Copyright © 2018 Juan Armond. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
